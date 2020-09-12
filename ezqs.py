b2a107e5d82095f9eeb5334f6f9d4b2baea92d6d
ebiak <ebrahim.amiri.k@gmail.com>
2020-09-12 14:16:37 +0430

    modify yes/no prompts

--- ezqs.py
+++ ezqs.py
@@ -4,8 +4,11 @@ import urllib.request
 import os.path
 import zipfile
 import subprocess
-def findWindowsLink(versionHTMLFile):
-    link = subprocess.Popen(["grep -o 'https://minecraft.azureedge.net/bin-win/[^\"]*' {}".format(versionHTMLFile)]
+def findWindowsLink():
+    if os.path.exists("./version.html"):
+        os.remove("./version.html")
+    urllib.request.urlretrieve("https://minecraft.net/en-us/download/server/bedrock/","./version.html")
+    link = subprocess.Popen(["grep -o 'https://minecraft.azureedge.net/bin-win/[^\"]*' {}".format("./version.html")]
         ,stdout=subprocess.PIPE,shell=True,universal_newlines=True)
     linkName = link.stdout.read().rsplit()[0]
     fileName = linkName.split("/")
@@ -14,11 +17,10 @@ def inputYN(prompt):
     # input prompt with a yes/no
     while True:
         inputData = input(prompt+": ")
-        yn = input(prompt+" = "+ inputData +" [Y,n]")
-        if yn == "" or yn == "y" or yn == "Y":
-            break
-        else:
+        if input(prompt+" = "+ inputData +" [Y,n]") in ("N","n"):
             continue
+        else:
+            break
     return inputData
 serverName = inputYN("server name")
 print("downloading elementZero...")
@@ -28,8 +30,7 @@ if not os.path.exists("./ez.zip"):
     urllib.request.urlretrieve("https://github.com/Element-0/ElementZero/releases/download/v0.1.0-6/ElementZero-v0.1.0-6-win64.zip","./ez.zip")
 else:
     print("file already downloaded!")
-    yn = input("do you want to redownload? [y,N]")
-    if yn == "y" or yn == "Y":
+    if input("do you want to redownload? [y,N]") in ("y", "Y"):
         urllib.request.urlretrieve("https://github.com/Element-0/ElementZero/releases/download/v0.1.0-6/ElementZero-v0.1.0-6-win64.zip","./ez.zip")
         print("done!")
 if os.path.exists("./"+serverName):
@@ -42,11 +43,8 @@ else:
         if fileName.startswith("ElementZero"):
             os.rename(fileName,serverName)
     print("done!")
-if os.path.exists("./version.html"):
-    os.remove("./version.html")
 print("checking for the latest minecraft version")
-urllib.request.urlretrieve("https://minecraft.net/en-us/download/server/bedrock/","./version.html")
-link, fileName = findWindowsLink("./version.html")
+link, fileName = findWindowsLink()
 if os.path.exists(fileName):
     print("\nlatest BDS is already downloaded!")
 else:
